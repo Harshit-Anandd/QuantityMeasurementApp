1c9d1c2302029f9bf045f54e12cbc8661b0db212
Anand K <harshit.anandd@gmail.com>
2026-02-24 10:12:52 +0530

    [Harshit] : added QuantityMeasurementAppTest.java in UC04-YardEquality

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -7,27 +7,80 @@ import com.apps.quantitymeasurement.QuantityMeasurementApp;
 
 public class QuantityMeasurementAppTest {
 	@Test
-	void testSameUnitEquality() {
+	void testFeetEquality() {
 
-		QuantityMeasurementApp.QuantityLength firstMeasurement =
+		QuantityMeasurementApp.QuantityLength first =
 				new QuantityMeasurementApp.QuantityLength(
 						2.0,
 						QuantityMeasurementApp.LengthUnit.FEET
 						);
 
-		QuantityMeasurementApp.QuantityLength secondMeasurement =
+		QuantityMeasurementApp.QuantityLength second =
 				new QuantityMeasurementApp.QuantityLength(
 						2.0,
 						QuantityMeasurementApp.LengthUnit.FEET
 						);
 
-		boolean result = firstMeasurement.equals(secondMeasurement);
-
-		assertTrue(result);
+		assertTrue(first.equals(second));
+		assertEquals(first.hashCode(), second.hashCode());
 	}
 
 	@Test
-	void testCrossUnitEquality() {
+	void testInchEquality() {
+
+		QuantityMeasurementApp.QuantityLength first =
+				new QuantityMeasurementApp.QuantityLength(
+						10.0,
+						QuantityMeasurementApp.LengthUnit.INCH
+						);
+
+		QuantityMeasurementApp.QuantityLength second =
+				new QuantityMeasurementApp.QuantityLength(
+						10.0,
+						QuantityMeasurementApp.LengthUnit.INCH
+						);
+
+		assertTrue(first.equals(second));
+	}
+
+	@Test
+	void testYardEquality() {
+
+		QuantityMeasurementApp.QuantityLength first =
+				new QuantityMeasurementApp.QuantityLength(
+						3.0,
+						QuantityMeasurementApp.LengthUnit.YARD
+						);
+
+		QuantityMeasurementApp.QuantityLength second =
+				new QuantityMeasurementApp.QuantityLength(
+						3.0,
+						QuantityMeasurementApp.LengthUnit.YARD
+						);
+
+		assertTrue(first.equals(second));
+	}
+
+	@Test
+	void testCentimeterEquality() {
+
+		QuantityMeasurementApp.QuantityLength first =
+				new QuantityMeasurementApp.QuantityLength(
+						25.0,
+						QuantityMeasurementApp.LengthUnit.CENTIMETER
+						);
+
+		QuantityMeasurementApp.QuantityLength second =
+				new QuantityMeasurementApp.QuantityLength(
+						25.0,
+						QuantityMeasurementApp.LengthUnit.CENTIMETER
+						);
+
+		assertTrue(first.equals(second));
+	}
+
+	@Test
+	void testFeetAndInchesEquality() {
 
 		QuantityMeasurementApp.QuantityLength oneFoot =
 				new QuantityMeasurementApp.QuantityLength(
@@ -41,29 +94,98 @@ public class QuantityMeasurementAppTest {
 						QuantityMeasurementApp.LengthUnit.INCH
 						);
 
-		boolean result = oneFoot.equals(twelveInches);
-
-		assertTrue(result);
+		assertTrue(oneFoot.equals(twelveInches));
+		assertTrue(twelveInches.equals(oneFoot));  // symmetry
 	}
 
 	@Test
-	void testInequality() {
+	void testYardAndFeetEquality() {
 
-		QuantityMeasurementApp.QuantityLength firstMeasurement =
+		QuantityMeasurementApp.QuantityLength oneYard =
+				new QuantityMeasurementApp.QuantityLength(
+						1.0,
+						QuantityMeasurementApp.LengthUnit.YARD
+						);
+
+		QuantityMeasurementApp.QuantityLength threeFeet =
+				new QuantityMeasurementApp.QuantityLength(
+						3.0,
+						QuantityMeasurementApp.LengthUnit.FEET
+						);
+
+		assertTrue(oneYard.equals(threeFeet));
+	}
+
+	@Test
+	void testYardAndInchEquality() {
+
+		QuantityMeasurementApp.QuantityLength oneYard =
+				new QuantityMeasurementApp.QuantityLength(
+						1.0,
+						QuantityMeasurementApp.LengthUnit.YARD
+						);
+
+		QuantityMeasurementApp.QuantityLength thirtySixInches =
+				new QuantityMeasurementApp.QuantityLength(
+						36.0,
+						QuantityMeasurementApp.LengthUnit.INCH
+						);
+
+		assertTrue(oneYard.equals(thirtySixInches));
+	}
+
+	@Test
+	void testCentimeterAndInchEquality() {
+
+		QuantityMeasurementApp.QuantityLength twoPointFiveFourCm =
+				new QuantityMeasurementApp.QuantityLength(
+						2.54,
+						QuantityMeasurementApp.LengthUnit.CENTIMETER
+						);
+
+		QuantityMeasurementApp.QuantityLength oneInch =
+				new QuantityMeasurementApp.QuantityLength(
+						1.0,
+						QuantityMeasurementApp.LengthUnit.INCH
+						);
+
+		assertTrue(twoPointFiveFourCm.equals(oneInch));
+	}
+
+	@Test
+	void testDifferentValuesNotEqual() {
+
+		QuantityMeasurementApp.QuantityLength first =
 				new QuantityMeasurementApp.QuantityLength(
 						1.0,
 						QuantityMeasurementApp.LengthUnit.FEET
 						);
 
-		QuantityMeasurementApp.QuantityLength secondMeasurement =
+		QuantityMeasurementApp.QuantityLength second =
 				new QuantityMeasurementApp.QuantityLength(
 						2.0,
 						QuantityMeasurementApp.LengthUnit.FEET
 						);
 
-		boolean result = firstMeasurement.equals(secondMeasurement);
+		assertFalse(first.equals(second));
+	}
 
-		assertFalse(result);
+	@Test
+	void testFeetAndCentimeterNotEqual() {
+
+		QuantityMeasurementApp.QuantityLength oneFoot =
+				new QuantityMeasurementApp.QuantityLength(
+						1.0,
+						QuantityMeasurementApp.LengthUnit.FEET
+						);
+
+		QuantityMeasurementApp.QuantityLength thirtyCm =
+				new QuantityMeasurementApp.QuantityLength(
+						30.0,
+						QuantityMeasurementApp.LengthUnit.CENTIMETER
+						);
+
+		assertFalse(oneFoot.equals(thirtyCm));
 	}
 
 	@Test
@@ -75,13 +197,11 @@ public class QuantityMeasurementAppTest {
 						QuantityMeasurementApp.LengthUnit.INCH
 						);
 
-		boolean result = measurement.equals(null);
-
-		assertFalse(result);
+		assertFalse(measurement.equals(null));
 	}
 
 	@Test
-	void testDifferentTypeComparison() {
+	void testDifferentObjectType() {
 
 		QuantityMeasurementApp.QuantityLength measurement =
 				new QuantityMeasurementApp.QuantityLength(
@@ -89,10 +209,8 @@ public class QuantityMeasurementAppTest {
 						QuantityMeasurementApp.LengthUnit.INCH
 						);
 
-		String invalidObject = "Invalid Type";
+		String invalidObject = "Invalid";
 
-		boolean result = measurement.equals(invalidObject);
-
-		assertFalse(result);
+		assertFalse(measurement.equals(invalidObject));
 	}
 }
