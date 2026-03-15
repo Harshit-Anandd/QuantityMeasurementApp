d6e08d857f32b4f4670614946352de5d4202090b
Anand K <harshit.anandd@gmail.com>
2026-03-16 02:31:32 +0530

    [Harshit] : modified QuantityMeasurementServiceImpl.java in service

--- src/main/java/com/apps/quantitymeasurement/service/QuantityMeasurementServiceImpl.java
+++ src/main/java/com/apps/quantitymeasurement/service/QuantityMeasurementServiceImpl.java
@@ -1,19 +1,32 @@
 package com.apps.quantitymeasurement.service;
 
 import com.apps.quantitymeasurement.dto.QuantityDTO;
+import com.apps.quantitymeasurement.entity.QuantityMeasurementEntity;
+import com.apps.quantitymeasurement.exception.QuantityMeasurementException;
+import com.apps.quantitymeasurement.repository.IQuantityMeasurementRepository;
 
-public class QuantityMeasurementServiceImpl
-implements IQuantityMeasurementService {
+public class QuantityMeasurementServiceImpl implements IQuantityMeasurementService {
 
 	private static final double EPSILON = 0.0001;
 
+	private final IQuantityMeasurementRepository repository;
+
+	public QuantityMeasurementServiceImpl(
+			IQuantityMeasurementRepository repository) {
+		this.repository = repository;
+	}
+
+	private void validateTypes(QuantityDTO q1, QuantityDTO q2) {
+
+		if (!q1.getUnit().getClass()
+				.equals(q2.getUnit().getClass()))
+			throw new QuantityMeasurementException(
+					"Incompatible measurement types.");
+	}
+
 	public boolean compare(QuantityDTO q1, QuantityDTO q2) {
 
-		if (q1 == null || q2 == null)
-			throw new NullPointerException("Quantity cannot be null");
-
-		if (!q1.getUnit().getClass().equals(q2.getUnit().getClass()))
-			return false;
+		validateTypes(q1, q2);
 
 		double base1 = q1.getUnit().convertToBaseUnit(q1.getValue());
 		double base2 = q2.getUnit().convertToBaseUnit(q2.getValue());
@@ -23,25 +36,57 @@ implements IQuantityMeasurementService {
 
 	public double add(QuantityDTO q1, QuantityDTO q2) {
 
-		double base1 = q1.getUnit().convertToBaseUnit(q1.getValue());
-		double base2 = q2.getUnit().convertToBaseUnit(q2.getValue());
+		validateTypes(q1, q2);
 
-		return base1 + base2;
+		double result =
+				q1.getUnit().convertToBaseUnit(q1.getValue())
+				+ q2.getUnit().convertToBaseUnit(q2.getValue());
+
+		repository.saveOperation(new QuantityMeasurementEntity(
+				"ADD",
+				q1.getValue()+" "+q1.getUnit().getUnitName(),
+				q2.getValue()+" "+q2.getUnit().getUnitName(),
+				String.valueOf(result)));
+
+		return result;
 	}
 
 	public double subtract(QuantityDTO q1, QuantityDTO q2) {
 
-		double base1 = q1.getUnit().convertToBaseUnit(q1.getValue());
-		double base2 = q2.getUnit().convertToBaseUnit(q2.getValue());
+		validateTypes(q1, q2);
 
-		return base1 - base2;
+		double result =
+				q1.getUnit().convertToBaseUnit(q1.getValue())
+				- q2.getUnit().convertToBaseUnit(q2.getValue());
+
+		repository.saveOperation(new QuantityMeasurementEntity(
+				"SUBTRACT",
+				q1.getValue()+" "+q1.getUnit().getUnitName(),
+				q2.getValue()+" "+q2.getUnit().getUnitName(),
+				String.valueOf(result)));
+
+		return result;
 	}
 
 	public double divide(QuantityDTO q1, QuantityDTO q2) {
 
-		double base1 = q1.getUnit().convertToBaseUnit(q1.getValue());
-		double base2 = q2.getUnit().convertToBaseUnit(q2.getValue());
+		validateTypes(q1, q2);
 
-		return base1 / base2;
+		double base2 =
+				q2.getUnit().convertToBaseUnit(q2.getValue());
+
+		if(Math.abs(base2) < EPSILON)
+			throw new QuantityMeasurementException("Division by zero");
+
+		double result =
+				q1.getUnit().convertToBaseUnit(q1.getValue()) / base2;
+
+		repository.saveOperation(new QuantityMeasurementEntity(
+				"DIVIDE",
+				q1.getValue()+" "+q1.getUnit().getUnitName(),
+				q2.getValue()+" "+q2.getUnit().getUnitName(),
+				String.valueOf(result)));
+
+		return result;
 	}
 }
