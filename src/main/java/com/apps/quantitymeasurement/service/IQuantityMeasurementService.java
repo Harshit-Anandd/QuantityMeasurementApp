5ae706ae0fa3cc37452c6ff3cdc5f364d74e6c2e
Anand K <harshit.anandd@gmail.com>
2026-03-16 00:01:58 +0530

    [Harshit] : added IQuantityMeasurementService.java in service

---
+++ src/main/java/com/apps/quantitymeasurement/service/IQuantityMeasurementService.java
@@ -0,0 +1,14 @@
+package com.apps.quantitymeasurement.service;
+
+import com.apps.quantitymeasurement.dto.QuantityDTO;
+
+public interface IQuantityMeasurementService {
+
+	boolean compare(QuantityDTO q1, QuantityDTO q2);
+
+	double add(QuantityDTO q1, QuantityDTO q2);
+
+	double subtract(QuantityDTO q1, QuantityDTO q2);
+
+	double divide(QuantityDTO q1, QuantityDTO q2);
+}
