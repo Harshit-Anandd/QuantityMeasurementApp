ee4bd17071ea495e4ce41c97d4e828d7e63b6164
Anand K <harshit.anandd@gmail.com>
2026-03-16 02:31:31 +0530

    [Harshit] : modified TemperatureUnit.java in model

--- src/main/java/com/apps/quantitymeasurement/model/TemperatureUnit.java
+++ src/main/java/com/apps/quantitymeasurement/model/TemperatureUnit.java
@@ -9,12 +9,9 @@ public enum TemperatureUnit implements IMeasurable {
 	public double convertToBaseUnit(double value) {
 
 		switch (this) {
-		case KELVIN:
-			return value;
-		case CELSIUS:
-			return value + 273.15;
-		case FAHRENHEIT:
-			return (value - 32) * 5 / 9 + 273.15;
+		case KELVIN: return value;
+		case CELSIUS: return value + 273.15;
+		case FAHRENHEIT: return (value - 32) * 5/9 + 273.15;
 		}
 
 		throw new IllegalStateException();
@@ -23,24 +20,20 @@ public enum TemperatureUnit implements IMeasurable {
 	public double convertFromBaseUnit(double baseValue) {
 
 		switch (this) {
-		case KELVIN:
-			return baseValue;
-		case CELSIUS:
-			return baseValue - 273.15;
-		case FAHRENHEIT:
-			return (baseValue - 273.15) * 9 / 5 + 32;
+		case KELVIN: return baseValue;
+		case CELSIUS: return baseValue - 273.15;
+		case FAHRENHEIT: return (baseValue - 273.15) * 9/5 + 32;
 		}
 
 		throw new IllegalStateException();
 	}
 
 	public double getConversionFactor() {
-		throw new UnsupportedOperationException(
-				"Temperature uses offset conversion.");
+		throw new UnsupportedOperationException();
 	}
 
-	public String getUnitName() {
-		return name();
+	public String getUnitName() { 
+		return name(); 
 	}
 
 	@Override
