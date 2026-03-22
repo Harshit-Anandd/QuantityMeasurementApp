1020b98c3210550f1d73914b32afcf359e4c3f25
Anand K <harshit.anandd@gmail.com>
2026-03-23 04:14:06 +0530

    [Harshit Anandd] : deleted IQuantityMeasurementRepository.java from repository

--- src/main/java/com/apps/quantitymeasurement/repository/IQuantityMeasurementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.apps.quantitymeasurement.repository;
-
-import com.apps.quantitymeasurement.entity.QuantityMeasurementEntity;
-
-public interface IQuantityMeasurementRepository {
-
-    void saveOperation(QuantityMeasurementEntity entity);
-}
