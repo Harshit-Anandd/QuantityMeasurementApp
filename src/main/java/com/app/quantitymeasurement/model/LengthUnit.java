b55cee6577521a6061ce489aac0e7ef028ebd24e
Anand K <harshit.anandd@gmail.com>
2026-03-23 04:16:12 +0530

    [Harshit Anandd] : added LengthUnit.java in model

---
+++ src/main/java/com/app/quantitymeasurement/model/LengthUnit.java
@@ -0,0 +1,31 @@
+package com.app.quantitymeasurement.model;
+
+public enum LengthUnit implements IMeasurable {
+
+	FEET(1.0),
+	INCH(1.0 / 12.0),
+	YARD(3.0),
+	CENTIMETER(1.0 / 30.48);
+
+	private final double factor;
+
+	LengthUnit(double factor) {
+		this.factor = factor;
+	}
+
+	public double convertToBaseUnit(double value) {
+		return value * factor;
+	}
+
+	public double convertFromBaseUnit(double baseValue) {
+		return baseValue / factor;
+	}
+
+	public double getConversionFactor() { 
+		return factor; 
+	}
+
+	public String getUnitName() { 
+		return name(); 
+	}
+}
