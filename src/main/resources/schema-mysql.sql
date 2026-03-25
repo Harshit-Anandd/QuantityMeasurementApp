f684474af47d13150410dbe2e2507dbf303e7648
Anand K <harshit.anandd@gmail.com>
2026-03-25 12:51:13 +0530

    [Harshit Anandd] : added schema-mysql.sql in resources

---
+++ src/main/resources/schema-mysql.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS quantity_measurement_entity (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    this_value DOUBLE NOT NULL,
+    this_unit VARCHAR(255) NOT NULL,
+    this_measurement_type VARCHAR(255) NOT NULL,
+    that_value DOUBLE NOT NULL,
+    that_unit VARCHAR(255) NOT NULL,
+    that_measurement_type VARCHAR(255) NOT NULL,
+    operation VARCHAR(255) NOT NULL,
+    result_value DOUBLE NULL,
+    result_unit VARCHAR(255) NULL,
+    result_measurement_type VARCHAR(255) NULL,
+    result_string VARCHAR(255) NULL,
+    is_error TINYINT(1) NULL DEFAULT 0,
+    error_message VARCHAR(255) NULL,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX idx_operation (operation),
+    INDEX idx_measurement_type (this_measurement_type),
+    INDEX idx_created_at (created_at)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
