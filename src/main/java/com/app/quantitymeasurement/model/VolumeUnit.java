0250e07b88eec1f3730c78af34072f4a08b9905a
Anand K <harshit.anandd@gmail.com>
2026-03-25 12:51:10 +0530

    [Harshit Anandd] : modified VolumeUnit.java in model

--- src/main/java/com/app/quantitymeasurement/model/VolumeUnit.java
+++ src/main/java/com/app/quantitymeasurement/model/VolumeUnit.java
@@ -3,7 +3,7 @@ package com.app.quantitymeasurement.model;
 public enum VolumeUnit implements IMeasurable {
 
 	LITRE(1),
-	MILLILITRE(0.001),
+	MILLILITER(0.001),
 	GALLON(3.78541);
 
 	private final double factor;
