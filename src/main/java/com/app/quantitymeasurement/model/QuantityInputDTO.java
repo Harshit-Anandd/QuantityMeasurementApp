77da4401b72a4336c7634455478541f76be6d0a3
Anand K <harshit.anandd@gmail.com>
2026-03-25 12:51:09 +0530

    [Harshit Anandd] : modified QuantityInputDTO.java in model

--- src/main/java/com/app/quantitymeasurement/model/QuantityInputDTO.java
+++ src/main/java/com/app/quantitymeasurement/model/QuantityInputDTO.java
@@ -1,20 +1,55 @@
 package com.app.quantitymeasurement.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@Schema(example = """
+        {
+          "thisQuantityDTO": {"value": 1.0, "unit": "FEET", "measurementType": "LengthUnit"},
+          "thatQuantityDTO": {"value": 12.0, "unit": "INCHES", "measurementType": "LengthUnit"},
+          "targetQuantityDTO": {"value": 0.0, "unit": "INCHES", "measurementType": "LengthUnit"}
+        }
+        """)
 public class QuantityInputDTO {
 
-    @NotNull(message = "First quantity is required")
+    @NotNull(message = "First quantity cannot be null")
     @Valid
-    private QuantityDTO thisQuantity;
+    private QuantityDTO thisQuantityDTO;
 
-    @NotNull(message = "Second quantity is required")
+    @NotNull(message = "Second quantity cannot be null")
     @Valid
-    private QuantityDTO thatQuantity;
+    private QuantityDTO thatQuantityDTO;
 
     @Valid
-    private QuantityDTO targetUnit;
+    @Schema(nullable = true)
+    @JsonAlias("targetUnitDTO")
+    private QuantityDTO targetQuantityDTO;
+
+    public QuantityDTO getThisQuantityDTO() {
+        return thisQuantityDTO;
+    }
+
+    public void setThisQuantityDTO(QuantityDTO thisQuantityDTO) {
+        this.thisQuantityDTO = thisQuantityDTO;
+    }
+
+    public QuantityDTO getThatQuantityDTO() {
+        return thatQuantityDTO;
+    }
+
+    public void setThatQuantityDTO(QuantityDTO thatQuantityDTO) {
+        this.thatQuantityDTO = thatQuantityDTO;
+    }
+
+    public QuantityDTO getTargetQuantityDTO() {
+        return targetQuantityDTO;
+    }
+
+    public void setTargetQuantityDTO(QuantityDTO targetQuantityDTO) {
+        this.targetQuantityDTO = targetQuantityDTO;
+    }
 }
