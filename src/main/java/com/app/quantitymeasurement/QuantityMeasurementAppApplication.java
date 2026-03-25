ca0e0d45e886af1478374c7fb352e73538386093
Anand K <harshit.anandd@gmail.com>
2026-03-25 14:33:24 +0530

    [Harshit] : modified QuantityMeasurementAppApplication.java in quantitymeasurement

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementAppApplication.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementAppApplication.java
@@ -1,9 +1,20 @@
 package com.app.quantitymeasurement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Quantity Measurements API",
+                version = "v1",
+                description = "REST API for quantity measurement operations"
+        ),
+        servers = @Server(url = "http://localhost:8080", description = "Generated server url")
+)
 public class QuantityMeasurementAppApplication {
 
     public static void main(String[] args) {
