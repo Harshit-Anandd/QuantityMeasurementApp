0a55e5db5809e518f56a0cea475d3d7a5b090a56
Anand K <harshit.anandd@gmail.com>
2026-03-30 03:22:29 +0530

    [Harshit] : modified QuantityMeasurementAppApplication.java in quantitymeasurement

--- src/main/java/com/app/quantitymeasurement/QuantityMeasurementAppApplication.java
+++ src/main/java/com/app/quantitymeasurement/QuantityMeasurementAppApplication.java
@@ -1,7 +1,10 @@
 package com.app.quantitymeasurement;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +16,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 version = "v1",
                 description = "REST API for quantity measurement operations"
         ),
+        security = @SecurityRequirement(name = "bearerAuth"),
         servers = @Server(url = "http://localhost:8080", description = "Generated server url")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class QuantityMeasurementAppApplication {
 
